7d9b7294bee75990fea50d88aa356d7b8d294432
pixel <alex.pixelwinner@gmail.com>
2024-04-11 13:58:40 +0300

    Message flow update

--- src/pages/Chat/components/Messages/components/DetailsTooltip.tsx
+++ src/pages/Chat/components/Messages/components/DetailsTooltip.tsx
@@ -1,34 +1,23 @@
 import React, { FC } from "react";
 import { Transaction } from "@utils/typings/types/common.types";
-import { Box, Tooltip, Typography } from "@mui/material";
-import InfoIcon from "@mui/icons-material/Info";
+import { Box, Tooltip, TooltipProps, Typography } from "@mui/material";
 import styled from "styled-components";
 import { format } from "date-fns";
 import { DATE_TIME_FORMAT } from "@utils/constants/common.constants";
-
-
-const TooltipContent = styled(Box)`
-    display: flex;
-    flex-direction: column;
-    height: max-content;
-    gap: 4px`;
 
 const TextContainer = styled(Box)`
     display: flex;
     flex-direction: column;
 `;
 
-
 type DetailsProps = {
-    transactions: Transaction[]
-}
+    transaction: Transaction,
+} & Omit<TooltipProps, "title">
 
-const Details: FC<DetailsProps> = ({ transactions }) => {
-    if (transactions.length === 0) {
-        return null;
-    }
+const DetailsTooltip: FC<DetailsProps> = ({ transaction, ...rest }) => {
+    const { children, ...otherProps } = rest;
 
-    const details = transactions.map((transaction) => (
+    const details = (
         <TextContainer key={transaction.txId}>
             <Typography variant="subtitle1">Tx id: {transaction.txId}</Typography>
             <Typography variant="subtitle1">From: {transaction.from}</Typography>
@@ -36,13 +25,13 @@ const Details: FC<DetailsProps> = ({ transactions }) => {
             <Typography variant="subtitle1">Value: {transaction.value} ETH</Typography>
             <Typography variant="subtitle1">Date: {format(new Date(transaction.date), DATE_TIME_FORMAT.shortDate)}</Typography>
         </TextContainer>
-    ));
+    );
 
     return (
-        <Tooltip placement="top" title={<TooltipContent>{details}</TooltipContent>}>
-            <InfoIcon color="primary" />
+        <Tooltip placement="top" arrow title={details} {...otherProps}>
+            {children}
         </Tooltip>
     );
 };
 
-export default Details;
+export default DetailsTooltip;
--- src/pages/Chat/components/Messages/components/Message.tsx
+++ src/pages/Chat/components/Messages/components/Message.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from "react";
-import { TMessage } from "@utils/typings/types/common.types";
+import { TMessage, Transaction } from "@utils/typings/types/common.types";
 import styled from "styled-components";
 import { Box, Typography } from "@mui/material";
 import { MessageType } from "@utils/typings/enums/common.enums";
 import { useAppContext } from "@utils/providers/AppProvider";
-import Details from "@pages/Chat/components/Messages/components/Details";
+import DetailsTooltip from "@pages/Chat/components/Messages/components/DetailsTooltip";
 
 const Container = styled(Box)<{ $messageType: MessageType, $isMine: boolean }>`
     display: flex;
@@ -21,41 +21,31 @@ const TextWrapper = styled(Box)<{ $messageType: MessageType, $isMine: boolean }>
     border: ${({ $messageType }) => $messageType === MessageType.SYSTEM ? "none" : "grey 1px solid"};
 `;
 
-const ContentWrapper = styled(Box)<{ $isMine: boolean }>`
-    display: flex;
-    flex-direction: ${({ $isMine }) => $isMine ? "row" : "row-reverse"};
-    gap: 4px`;
-
-const UserDetails = styled(Box)`
+const UserDetails = styled(Box)<{ $isMine: boolean }>`
     display: flex;
     flex-direction: row;
     align-items: center;
-    gap: 4px`;
-
+    gap: 4px;
+    justify-content: ${({ $isMine }) => ($isMine ? "flex-end" : "flex-start")};
+`;
 
 const Message: FC<TMessage> = ({ sender, text, type, transactions, userId }) => {
     const { userId: localUserId } = useAppContext();
     const isMine = localUserId === userId;
-    const align = isMine ? "right" : "left";
     const hiddenId = hideId(userId);
 
 
     return (
         <Container $messageType={type} $isMine={isMine}>
-            <UserDetails>
-                <Typography align={align} variant="body2">{sender}</Typography>
+            <UserDetails $isMine={isMine}>
+                <Typography variant="body2">{sender}</Typography>
 
                 {type === MessageType.USER && <Typography color="text.secondary" variant="subtitle2"> | {hiddenId}</Typography>}
             </UserDetails>
 
-
-            <ContentWrapper $isMine={isMine}>
-                <Details transactions={transactions} />
-
-                <TextWrapper $messageType={type} $isMine={isMine}>
-                    <Typography variant="body1">{text}</Typography>
-                </TextWrapper>
-            </ContentWrapper>
+            <TextWrapper $messageType={type} $isMine={isMine}>
+                <Typography variant="body1">{renderTransactionText(text, transactions)}</Typography>
+            </TextWrapper>
         </Container>
     );
 };
@@ -78,4 +68,28 @@ const getAlign = (messageType: MessageType, isMine: boolean): string => {
 const hideId = (id: string) => {
     const visiblePart = id.substring(0, 8);
     return `${visiblePart}***`;
+};
+
+const renderTransactionText = (text: string, transactions: Transaction[]) => {
+    const transactionRegex = new RegExp(`(${transactions.map(t => t.txId).join("|")})`, "g");
+    const partsWithTokens = text.split(transactionRegex).filter(part => part);
+
+    return partsWithTokens.map((part, index) => {
+        const transaction = transactions.find(t => t.txId === part);
+
+        const txId = `tx-${index}`;
+        const textId = `text-${index}`;
+
+        if (!transaction) {
+            return <Typography component="span" variant="body2" key={textId}>{part}</Typography>;
+        }
+
+        return (
+            <DetailsTooltip key={txId} transaction={transaction}>
+                <Typography component="span" variant="body2" fontWeight="bold" style={{ textDecoration: "underline", cursor: "pointer" }}>
+                    {part}
+                </Typography>
+            </DetailsTooltip>
+        );
+    });
 };
